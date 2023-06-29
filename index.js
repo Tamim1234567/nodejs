080ceab310647fdf9e464feca62c3e1701d61ef0
Tamim <86140595+Tamim1234567@users.noreply.github.com>
2023-06-29 09:17:30 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const http = require('http');
-const PORT = 3000;
+// Node server which will handle socket io connections
+const io = require('socket.io')(8000)
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
+const users = {};
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+io.on('connection', socket =>{
+    // If any new user joins, let other users connected to the server know!
+    socket.on('new-user-joined', name =>{ 
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    // If someone sends a message, broadcast it to other people
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    });
+
+    // If someone leaves the chat, let others know 
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+
+
+})
